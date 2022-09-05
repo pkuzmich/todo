274325eb0e744ce4fe1c0151513010f6dbb1cb34
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-09-05 15:27:38 +0200

    Adding and styling Delete and Complete task buttons

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,4 +1,5 @@
-import { RiTodoFill } from 'react-icons/ri';
+import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 export default function Todo({ todo, deleteTodo }) {
@@ -6,6 +7,8 @@ export default function Todo({ todo, deleteTodo }) {
     <li className={styles.item} onDoubleClick={() => deleteTodo(todo.id)}>
       <RiTodoFill className={styles.itemIcon} />
       <span className={styles.itemText}>{todo.text}</span>
+      <RiDeleteBin2Line className={styles.deleteIcon} />
+      <FaCheck className={styles.checkIcon} />
     </li>
   );
 }
