b92938596005f9b03ae49a0ddc7d12e18e2b89a0
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-09-05 09:33:39 +0200

    File structure organization

---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,5 @@
+import styles from './Todo.module.css';
+
+export default function Todo() {
+  return <div>Todo</div>;
+}
---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,5 @@
+import styles from './TodoForm.module.css';
+
+export default function TodoForm() {
+  return <div>TodoForm</div>;
+}
