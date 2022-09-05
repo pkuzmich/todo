69fdadf9da8bd0ef6e08679e1cc2398cccf0fcdf
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-09-05 13:37:28 +0200

    Adding an icon for a task

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './Todo.module.css';
 
 export default function Todo({ todo, index, deleteTodo }) {
   return (
     <li className={styles.item} onDoubleClick={() => deleteTodo(index)}>
+      <RiTodoFill className={styles.itemIcon} />
       <span className={styles.itemText}>{todo}</span>
     </li>
   );
