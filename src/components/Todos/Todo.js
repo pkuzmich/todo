d9ea6e37429fffcccd386ed27130ddaf04988b7f
Pavel Kuzmich <90804508+pkuzmich@users.noreply.github.com>
2022-09-05 12:13:43 +0200

    Changing a item task

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,8 +2,8 @@ import styles from './Todo.module.css';
 
 export default function Todo({ todo }) {
   return (
-    <li className={styles.todo}>
-      <span className={styles.todoText}>{todo}</span>
+    <li className={styles.item}>
+      <span className={styles.itemText}>{todo}</span>
     </li>
   );
 }
